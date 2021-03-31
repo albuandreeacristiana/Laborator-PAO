5f2def4eb6b6f57b0c46713c3732356bf67f98c3
Andreea-Cristiana Albu <albuandreeacristiana@gmail.com>
2021-03-31 20:59:18 +0300

    Add files via upload

---
+++ CatalogV1/src/Model/Student.java
@@ -0,0 +1,81 @@
+package Model;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class Student extends DateIdentificare {
+    private int an;
+    private int grupa;
+    private List<Nota> note;
+
+    public Student(Nume nume, Data dataNasterii, String adresa, String stareCivila, int an, int grupa, List<Nota> note) {
+        super(nume, dataNasterii, adresa, stareCivila);
+        this.an = an;
+        this.grupa = grupa;
+        this.note = note;
+    }
+
+    public Student() {
+    }
+
+    public int getAn() {
+        return an;
+    }
+
+    public void setAn(int an) {
+        this.an = an;
+    }
+
+    public int getGrupa() {
+        return grupa;
+    }
+
+    public void setGrupa(int grupa) {
+        this.grupa = grupa;
+    }
+
+    public List<Nota> getNote() {
+        return note;
+    }
+
+    public void setNote(List<Nota> note) {
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "nume=" + nume +
+                ", dataNasterii=" + dataNasterii +
+                ", adresa='" + adresa + '\'' +
+                ", stareCivila='" + stareCivila + '\'' +
+                ", an=" + an +
+                ", grupa=" + grupa +
+                ", note=" + note +
+                '}';
+    }
+
+    public static Comparator<Student> compStudent = new Comparator<Student>() {
+        @Override
+        public int compare(Student o1, Student o2) {
+            if(o1.getGrupa() != o2.getGrupa()){
+            return o1.getGrupa()- o2.getGrupa();
+            }
+            else{
+                if( o1.getNume().getNume().compareTo(o2.getNume().getNume()) != 0){
+                    return o1.getNume().getNume().compareTo(o2.getNume().getNume());
+                }
+                else{
+                    return o1.getNume().getPrenume().compareTo(o2.getNume().getPrenume());
+                }
+            }
+
+        }
+       /* public int compare(Materie o1, Materie o2) {
+            if(o1.semestru != o2.semestru)
+                return o1.semestru - o2.semestru;
+            else
+                return o1.numeMaterie.compareTo(o2.numeMaterie);
+        }*/
+    };
+}
---
+++ CatalogV1/src/Service/Services.java
@@ -0,0 +1,275 @@
+package Service;
+
+import Model.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import static Model.Student.compStudent;
+
+public class Services {
+    private List<Student> studenti = new ArrayList<Student>();
+    private List<Materie> materii = new ArrayList<Materie>();
+    private List<Profesor> profesori = new ArrayList<Profesor>();
+    private List<Grupa> grupe = new ArrayList<Grupa>();
+
+    public void addStudent(String nume, String initialaTatalui, String prenume,
+                           int zi, int luna, int an,
+                           String adresa, String stareCivila,
+                           int anStudiu, int nrGrupa){
+        Nume n = new Nume(nume, initialaTatalui, prenume);
+        Data dn = new Data(zi,luna,an);
+        List <Nota> note = new ArrayList<Nota>();
+        Student s = new Student(n,dn,adresa,stareCivila,anStudiu,nrGrupa, note);
+        studenti.add(s);
+        int found = 0;
+
+        for(int i = 0; i < grupe.size(); i++){
+            Grupa temp = grupe.get(i);
+            if(temp.getNr() == nrGrupa) {
+                found = 1;
+                temp.addStudent(s);
+            }
+        }
+        if(found != 1){
+            List<Student> l= new ArrayList<Student>();
+            Grupa temp = new Grupa(nrGrupa, l);
+            temp.addStudent(s);
+            grupe.add(temp);
+
+        }
+        Collections.sort(studenti ,compStudent);
+    }
+
+    public void addProfesor(String nume, String initialaTatalui, String prenume,
+                           int zi, int luna, int an,
+                           String adresa, String stareCivila,
+                           int zia, int lunaa, int ana, List<Materie> mat){
+        Nume n = new Nume(nume, initialaTatalui, prenume);
+        Data dn = new Data(zi,luna,an);
+        Data da = new Data(zia,lunaa,ana);
+        Profesor p= new Profesor(n,dn,adresa,stareCivila,da, mat);
+        profesori.add(p);
+        if (mat.size() > 0 && materii.size() > 0) {
+            for (int j = 0; j < mat.size(); j++) {
+                Materie search = mat.get(j);
+                if(searchMaterie(search.getDenumire()) == -1){
+                    materii.add(search);
+                }
+            }
+        }
+        else {
+            if(mat.size()>0) {
+                materii.addAll(mat);
+            }
+        }
+    }
+    public int addNota(Nume student, int valoare, int zi, int luna, int an, String materie, Nume profesor){
+        if(materii.size()==0){
+            System.out.println("Materia nu exista.");
+            return 0;
+        }
+        Student st = new Student();
+        Profesor prof = new Profesor();
+        Materie m = new Materie();
+        if(studenti.size()==0){
+            System.out.println("Studentul pe care incercati sa il punctati nu este inregistrat.");
+            return 0;
+        }
+        else{int found = 0;
+            for(int i = 0; i < studenti.size();i++){
+                st = studenti.get(i);
+                if(st.getNume().equals(student)){
+                    found = 1;
+                    break;
+                }
+            }
+            if(found == 0){
+                System.out.println("Studentul pe care incercati sa il punctati nu este inregistrat.");
+                return 0;
+            }
+        }
+        if(profesori.size()==0){
+            System.out.println("Profesorul nu este inregistrat.");
+            return 0;
+        }
+        else{
+            int found = 0;
+            for(int i = 0; i < profesori.size();i++){
+                prof = profesori.get(i);
+                if(prof.getNume().equals(profesor)){
+                    found = 1;
+                    break;
+                }
+            }
+            if(found == 0){
+                System.out.println("Profesorul nu este inregistrat.");
+                return 0;
+            }
+            else
+            {
+                int f = 0;
+                for(int i = 0; i <= prof.getMaterii().size(); i ++){
+                    m = prof.getMaterii().get(i);
+                    if(m.getDenumire().equals(materie)){
+                        f = 1;
+                        break;
+                    }
+                }
+                if(f == 0){
+                    System.out.println("Profesorul selectat nu preda aceasta materie!");
+                }
+                else{
+                    Nota n = new Nota(valoare,new Data(zi, luna, an),m, prof );
+                    st.getNote().add(n);
+                }
+            }
+        }
+
+    return 0;
+    }
+
+    public int searchStudent(Nume student){
+        if(studenti.size()>0){
+            for(int i = 0; i < studenti.size(); i++){
+                if(studenti.get(i).getNume().equals(student)){
+                    System.out.println(studenti.get(i));
+                    return i;
+                }
+            }
+            System.out.println("Studentul cautat nu a fost gasit!");
+            return -1;
+        }
+        else{
+            System.out.println("Nu exista studenti inregistrati.");
+            return -1;
+        }
+    }
+
+    public int searchProfesor(Nume profesor){
+        if(profesori.size()>0){
+            for(int i = 0; i < profesori.size(); i++){
+                if(profesori.get(i).getNume().equals(profesor)){
+                    System.out.println(profesori.get(i));
+                    return i;
+                }
+            }
+            System.out.println("Profesorul cautat nu a fost gasit!");
+            return -1;
+        }
+        else{
+            System.out.println("Nu exista profesori inregistrati.");
+            return -1;
+        }
+    }
+
+    public void medieStudent(Nume student){
+        Student temp = studenti.get(searchStudent(student));
+        float medie = 0;
+        if(temp.getNote().size()>0)
+        {
+            for (int i = 0; i < temp.getNote().size(); i++) {
+                medie = medie + temp.getNote().get(i).getValoare();
+            }
+            System.out.println("Media studentului " +
+                    student + " este " + medie / temp.getNote().size());
+        }
+        else{
+            System.out.println("Studentul nu are note!");
+        }
+    }
+
+    public int searchMaterie(String denumire){
+        if(materii.size()<1){
+            return -1;
+        }
+        else{
+            for(int i = 0; i < materii.size(); i++){
+                if(materii.get(i).getDenumire().equals(denumire)){
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+    public void addMaterie(Nume profesor, Materie materie, int add){
+        int index = searchProfesor(profesor);
+        if(index == -1){
+            System.out.println("Profesorul nu este inregistrat");
+        }
+        else{
+            if(!profesori.get(index).getMaterii().contains(materie)) {
+                profesori.get(index).getMaterii().add(materie);
+            }
+            if(add == 1){
+                materii.add(materie);
+            }
+        }
+    }
+
+    public int changeGrupa(Nume student, int grupa){
+        int index = searchStudent(student);
+        if(index == -1){
+            System.out.println("Studentul nu este inregistrat.");
+            return 0;
+        }
+        Student aux = studenti.get(index);
+        addStudent(aux.getNume().getNume(),aux.getNume().getInitialeleTatalui(),aux.getNume().getPrenume(),
+                aux.getDataNasterii().getZi(),aux.getDataNasterii().getLuna(), aux.getDataNasterii().getAn(),
+                aux.getAdresa(),aux.getStareCivila(),aux.getAn(), grupa);
+        studenti.get(studenti.size()-1).setNote(aux.getNote());
+        if(grupe.size()>0){
+            int pos = -1;
+            for(int i = 0; i < grupe.size(); i++){
+                if(grupe.get(i).getNr() == aux.getGrupa()){
+                    pos = i;
+                    break;
+                }
+            }
+            grupe.get(pos).getStudenti().remove(aux);
+            studenti.remove(aux);
+
+        }
+        Collections.sort(studenti ,compStudent);
+        return 0;
+    }
+
+    public int deleteStudent(Nume student){
+        int index = searchStudent(student);
+        if(index == -1){
+            System.out.println("Studentul nu este inregistrat.");
+            return 0;
+        }
+        Student aux = studenti.get(index);
+        int pos = -1;
+        for(int i = 0; i < grupe.size(); i++){
+            if(grupe.get(i).getNr() == aux.getGrupa()){
+                pos = i;
+                break;
+            }
+        }
+        grupe.get(pos).getStudenti().remove(aux);
+        studenti.remove(aux);
+        return 0;
+
+    }
+
+    public void deleteProfesor(Nume profesor){
+        profesori.remove(searchProfesor(profesor));
+    }
+
+    public List<Materie> getMaterii() {
+        return materii;
+    }
+
+    @Override
+    public String toString() {
+        return "Services{" +
+                "studenti=" + studenti +
+                ", materii=" + materii +
+                ", profesori=" + profesori +
+                ", grupe=" + grupe +
+                '}';
+    }
+}
